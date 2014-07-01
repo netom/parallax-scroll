aff831e6f8e2087bad9719034abf8dc05c031f93
Tamas Laszlo Fabian <tamas.fabian@vengit.com>
2014-07-01 14:03:27 +0200

    Refactor, use translate3d (faster)

--- parallax-scroll.js
+++ parallax-scroll.js
@@ -1,76 +1,98 @@
 var PS = {};
 
-PS.objects = $([]);
+// List of parallax objects
+PS.pobjs = $([]);
 
+// Window object wrapped in $
 PS.jqw = $(window);
 
-PS.adjustBackgrounds = function () {
-    PS.objects.each(function (i, e) {
-        var pw = e.img.parent().width();
+// get parallax value for a PO.
+// animation should be executed when it's between 0 and 1
+// 0: beginning to appear at the bottom,
+// 1: just disappeared at the top.
+PS.getPV = function (po) {
+    // The distance of the parallax element from the top of the page
+    var top = po.element.offset().top;
 
-        if (pw > e.minWidth && e.img.width() != pw) {
-            e.img.width(pw);
+    // The distance of the top of the visible region of the document
+    // from the top of the page
+    var visibleTop = PS.jqw.scrollTop();
+
+    // The distance of the bottom of the visible region of the document
+    // from the top of the page
+    var visibleBottom = visibleTop + PS.jqw.height();
+
+    // Return the parallax value.
+    // Can be negative, and larger than 1. This means that the
+    // element is out of the screen.
+    return (visibleBottom - top) / (visibleBottom - visibleTop + po.height);
+};
+
+PS.getFreedom = function (po) {
+    return po.img.height() - po.height;
+};
+
+PS.getMargin = function (po) {
+    var pv = PS.getPV(po);
+
+    if (po.reverse) {
+        pv = 1 - pv;
+    }
+
+    return -Math.round(pv * PS.getFreedom(po))
+};
+
+PS.adjustBackgrounds = function () {
+    PS.pobjs.each(function (i, po) {
+        var pw = po.img.parent().width();
+
+        if (pw > po.minWidth && po.img.width() != pw) {
+            po.img.width(pw);
         }
 
-        // The distance of the parallax element from the top of the page
-        var top = e.element.offset().top;
+        var pv = PS.getPV(po);
 
-        // The distance of the top of the visible region of the document
-        // from the top of the page
-        var visibleTop = PS.jqw.scrollTop();
-
-        // The distance of the bottom of the visible region of the document
-        // from the top of the page
-        var visibleBottom = visibleTop + PS.jqw.height();
-
-        if (top >= visibleTop - e.height && top <= visibleBottom) {
-            // pv: "parallax value": [0.0,1.0] 
-            // abstracts the progress of the animation
-            var pv = Math.min(1, Math.max(0, (visibleBottom - top) / (visibleBottom - visibleTop + e.height)));
-
-            if (e.reverse) {
-                pv = 1 - pv;
-            }
-
-            // The amount of freedome the image has
-            var freedom = e.img.height() - e.height;
-
-            // The amount of margin we should have
-            var margin = -Math.round(pv * freedom);
-
-            e.img.css({"margin-top": margin});
+        if (pv <= 1 && pv >= 0) {
+            po.margin = PS.getMargin(po, pv);
+            po.img.css({"transform": "translate3d(0px," + po.margin + "px,0px)"});
         }
     });
 };
 
 PS.init = function () {
-    $(".parallax-scroll").each(function (i, e) {
-        var jqe = $(e);
+
+    $(".parallax-scroll").each(function (i, pelement) {
+        var jqpelement = $(pelement);
 
         // Model object representing a parallax scrolling area
-        var o = {
-            "imgsrc": jqe.attr("data-image"),
-            "height": parseInt(jqe.attr("data-height")),
-            "minWidth": parseInt(jqe.attr("data-min-background-width")),
-            "element": jqe,
+        var po = {
+            "imgsrc": jqpelement.attr("data-image"),
+            "height": parseInt(jqpelement.attr("data-height")),
+            "minWidth": parseInt(jqpelement.attr("data-min-background-width")),
+            "element": jqpelement,
             "img": null,
-            "reverse": jqe.attr("data-reverse") != undefined,
-            "prevPv": -1
+            "reverse": jqpelement.attr("data-reverse") != undefined,
+            "margin": 0
         };
 
-        jqe.height(o.height);
+        jqpelement.height(po.height);
 
-        o.img = $(document.createElement("img"));
-        o.img.attr("src", o.imgsrc);
-        jqe.append(o.img);
+        var i = new Image();
+        i.src = po.imgsrc;
 
-        PS.objects.push(o);
+        //po.img = $(document.createElement("img"));
+        //po.img.attr("src", po.imgsrc);
+        po.img = $(i);
+
+        jqpelement.append(po.img);
+
+        PS.pobjs.push(po);
     });
 
-    $(window).resize(PS.adjustBackgrounds);
-    $(window).scroll(PS.adjustBackgrounds);
-
-    setTimeout(PS.adjustBackgrounds, 0);
+    PS.jqw.resize(PS.adjustBackgrounds);
+    PS.jqw.scroll(function (e) {e.preventDefault(); PS.adjustBackgrounds()});
+    
+    PS.adjustBackgrounds();
 };
 
 $(document).ready(function () {
